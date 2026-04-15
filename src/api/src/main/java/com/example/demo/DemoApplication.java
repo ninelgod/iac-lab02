4f2812aadbfbe37a256aa2dc529141dc4c16dd3e
ninelgod <emiialfa08@gmail.com>
2026-04-14 20:04:15 -0500

    fix: upload all

---
+++ src/api/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) throws Exception{
+		System.out.println("HEllo world");
+	}
+
+}
